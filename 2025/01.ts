2ca2abb54425b6326e6f7100e80a3f49a4a55ea7
Ross McNab <215740+rmcn@users.noreply.github.com>
2025-12-01 13:00:14 +0000

    2025 day 1

---
+++ 2025/01.ts
@@ -0,0 +1,60 @@
+import { Solution } from '../solution.ts'
+import * as R from 'remeda'
+import * as U from '../utls.ts'
+
+export default <Solution> {
+  one: (input: string) => {
+    const lines = U.lines(input)
+    let dial = 50
+    let zeroCount = 0
+    lines.forEach((line) => {
+      if (line[0] == 'L') {
+        dial -= U.int(line)
+        while (dial < 0) dial += 100
+      }
+      if (line[0] == 'R') {
+        dial += U.int(line)
+        while (dial > 99) dial -= 100
+      }
+
+      if (dial == 0) {
+        zeroCount++
+      }
+    })
+
+    return zeroCount
+  },
+
+  two: (input: string) => {
+    const lines = U.lines(input)
+    let dial = 50
+    let zeroCount = 0
+    lines.forEach((line) => {
+      const count = U.int(line)
+      if (line[0] == 'L') {
+        for (let i = 0; i < count; i++) {
+          dial--
+          if (dial < 0) {
+            dial += 100
+          }
+          if (dial == 0) {
+            zeroCount++
+          }
+        }
+      }
+      if (line[0] == 'R') {
+        for (let i = 0; i < count; i++) {
+          dial++
+          if (dial > 99) {
+            dial -= 100
+          }
+          if (dial == 0) {
+            zeroCount++
+          }
+        }
+      }
+    })
+
+    return zeroCount
+  },
+}
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { loadSolution, readExample, readInput } from './solution.ts'
 
-const year = 2021
-const day = 4
+const year = 2025
+const day = 1
 
 const solution = await loadSolution(year, day)
 const input = await readInput(year, day)
 const example = await readExample(year, day)
 
-console.log('One:', solution.one(example))
+console.log('One:', solution.one(input))
 console.log('Two:', solution.two(input))
