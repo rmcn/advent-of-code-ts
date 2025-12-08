a87405d5d09f1d1a7b03987ba9014c63e6c52eca
Ross McNab <215740+rmcn@users.noreply.github.com>
2025-12-08 17:29:39 +0000

    2025 day 8 refactor

--- 2025/08.ts
+++ 2025/08.ts
@@ -4,109 +4,32 @@ import * as U from '../utls.ts'
 
 export default <Solution> {
   one: (input: string) => {
-    const sockets: Socket[] = U.lines(input).map((v, i) => {
-      const [x, y, z] = U.ints(v)
-      return {
-        x,
-        y,
-        z,
-        i,
-        parent: i,
-        size: 1,
-      }
-    })
+    const sockets: Socket[] = parseSockets(input)
+    const pairs = pairsByDistance(sockets)
 
     const MERGE_COUNT = sockets.length <= 20 ? 10 : 1000
-
-    const pairs: { i: number; j: number; dSquared: number }[] = []
-    for (let i = 0; i < sockets.length; i++) {
-      const a = sockets[i]
-      for (let j = i + 1; j < sockets.length; j++) {
-        const b = sockets[j]
-        const dSquared = Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) +
-          Math.pow(a.z - b.z, 2)
-        pairs.push({ i, j, dSquared })
-      }
-    }
-
-    const sortedPairs = R.sortBy(pairs, R.prop('dSquared'))
     //console.log(sortedPairs.slice(0, MERGE_COUNT))
 
     for (let i = 0; i < MERGE_COUNT; i++) {
-      const pair = sortedPairs[i]
-      const rootA = root(sockets, pair.i)
-      const rootB = root(sockets, pair.j)
-      if (rootA == rootB) {
-        //console.log('Already merged')
-        continue
-      }
-
-      if (sockets[rootA].size >= sockets[rootB].size) {
-        //console.log(`Merging ${rootB} into ${rootA}`)
-        sockets[rootB].parent = rootA
-        sockets[rootA].size += sockets[rootB].size
-      } else {
-        //console.log(`Merging ${rootA} into ${rootB}`)
-        sockets[rootA].parent = rootB
-        sockets[rootB].size += sockets[rootA].size
-      }
+      const pair = pairs[i]
+      unionSets(sockets, pair.i, pair.j)
     }
 
-    const lengths = sockets.map((s) => s.size)
-    const longestThree = lengths.sort((a, b) => a - b).reverse().slice(0, 3)
-    return R.product(longestThree)
+    const sizes = sockets.filter((s, i) => s.parent == i).map((s) => s.size)
+    const biggestThree = sizes.sort((a, b) => b - a).slice(0, 3)
+    return R.product(biggestThree)
   },
 
   two: (input: string) => {
-    const sockets: Socket[] = U.lines(input).map((v, i) => {
-      const [x, y, z] = U.ints(v)
-      return {
-        x,
-        y,
-        z,
-        i,
-        parent: i,
-        size: 1,
-      }
-    })
+    const sockets: Socket[] = parseSockets(input)
+    const pairs = pairsByDistance(sockets)
 
-    const pairs: { i: number; j: number; dSquared: number }[] = []
-    for (let i = 0; i < sockets.length; i++) {
-      const a = sockets[i]
-      for (let j = i + 1; j < sockets.length; j++) {
-        const b = sockets[j]
-        const dSquared = Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) +
-          Math.pow(a.z - b.z, 2)
-        pairs.push({ i, j, dSquared })
-      }
-    }
+    for (let i = 0; i < pairs.length; i++) {
+      const pair = pairs[i]
+      const newRoot = unionSets(sockets, pair.i, pair.j)
 
-    const sortedPairs = R.sortBy(pairs, R.prop('dSquared'))
-    //console.log(sortedPairs.slice(0, MERGE_COUNT))
-
-    for (let i = 0; i < sortedPairs.length; i++) {
-      const pair = sortedPairs[i]
-      const rootA = root(sockets, pair.i)
-      const rootB = root(sockets, pair.j)
-      if (rootA == rootB) {
-        //console.log('Already merged')
-        continue
-      }
-
-      if (sockets[rootA].size >= sockets[rootB].size) {
-        //console.log(`Merging ${rootB} into ${rootA}`)
-        sockets[rootB].parent = rootA
-        sockets[rootA].size += sockets[rootB].size
-        if (sockets[rootA].size == sockets.length) {
-          return sockets[pair.i].x * sockets[pair.j].x
-        }
-      } else {
-        //console.log(`Merging ${rootA} into ${rootB}`)
-        sockets[rootA].parent = rootB
-        sockets[rootB].size += sockets[rootA].size
-        if (sockets[rootB].size == sockets.length) {
-          return sockets[pair.i].x * sockets[pair.j].x
-        }
+      if (sockets[newRoot].size == sockets.length) {
+        return sockets[pair.i].x * sockets[pair.j].x
       }
     }
   },
@@ -116,9 +39,36 @@ type Socket = {
   x: number
   y: number
   z: number
-  i: number
   parent: number
   size: number
+}
+
+function parseSockets(input: string): Socket[] {
+  return U.lines(input).map((v, i) => {
+    const [x, y, z] = U.ints(v)
+    return {
+      x,
+      y,
+      z,
+      parent: i,
+      size: 1,
+    }
+  })
+}
+
+function pairsByDistance(sockets: Socket[]) {
+  const pairs: { i: number; j: number; dSquared: number }[] = []
+  for (let i = 0; i < sockets.length; i++) {
+    const a = sockets[i]
+    for (let j = i + 1; j < sockets.length; j++) {
+      const b = sockets[j]
+      const dSquared = Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) +
+        Math.pow(a.z - b.z, 2)
+      pairs.push({ i, j, dSquared })
+    }
+  }
+
+  return R.sortBy(pairs, R.prop('dSquared'))
 }
 
 function root(sockets: Socket[], i: number): number {
@@ -127,4 +77,25 @@ function root(sockets: Socket[], i: number): number {
     root = sockets[root].parent
   }
   return root
+}
+
+function unionSets(sockets: Socket[], i: number, j: number): number {
+  const rootA = root(sockets, i)
+  const rootB = root(sockets, j)
+  if (rootA == rootB) {
+    //console.log('Already merged')
+    return rootA
+  }
+
+  if (sockets[rootA].size >= sockets[rootB].size) {
+    //console.log(`Merging ${rootB} into ${rootA}`)
+    sockets[rootB].parent = rootA
+    sockets[rootA].size += sockets[rootB].size
+    return rootA
+  } else {
+    //console.log(`Merging ${rootA} into ${rootB}`)
+    sockets[rootA].parent = rootB
+    sockets[rootB].size += sockets[rootA].size
+    return rootB
+  }
 }
